7bf393dae1058ff491d2bd448bf214f0b2509311
ibrahama <ibrahama2003@gmail.com>
2022-10-17 22:56:16 +0200

    dn

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
- import {  HiOutlineMenu, } from 'react-icons/hi';
+import {  HiOutlineMenu, } from 'react-icons/hi';
 import { RiCloseLine } from 'react-icons/ri';
 
-import { logo } from '../assets';
+// import { logo } from '../assets';
 import{links} from '../assets/constants'
 
 
 const NavLinks = ({ handleClick }) => (
+
   <div className="mt-10">
     {links.map((item) => (
       <NavLink
         key={item.name}
         to={item.to}
-        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-emerald-500"
         onClick={() => handleClick && handleClick()}
       >
         <item.icon className="w-6 h-6 mr-2" />
@@ -24,11 +25,13 @@ const NavLinks = ({ handleClick }) => (
 );
 
 const Sidebar = () => {
+    let logo = "https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"
+
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <div className="md:flex hidden flex-col w-[220px] py-10 px-4 bg-[black]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
       </div>
@@ -42,7 +45,7 @@ const Sidebar = () => {
         )}
       </div>
 
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[black] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
